83139912ac3c3f8a525bb8a1f45fe3144f59b602
Antone B. Cabral <antone.b.cabral@gmail.com>
2021-01-24 03:05:37 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const mixedMesssages = {
+
+  _messageData: {
+
+    intro: [
+      'Welcome padawan!',
+      'Welcome friend,'
+    ],
+    inter: [
+      'You have entered the eternal portal...',
+      'Take a seat...'
+    ],
+    end: [
+      'Would you like to play darts?',
+      'Would you like to play game?'
+    ]
+
+  },
+
+  getRandomMessage() {
+    let message = '';
+    for (prop in this._messageData) {
+      let index = Math.floor(Math.random() * this._messageData[prop].length);
+      message = message + ' ' + this._messageData[prop][index];
+    };
+
+    return message;
+  }
+
+
+}
+
+
+mixedMesssages.getRandomMessage();
